1a0483bd0548dcf57b47f1c03992aa16b2e34b06
alins666 <alinkhak23@gmail.com>
2023-11-29 19:04:29 +0700

    first commit

---
+++ rabota_1.1/zadaniya.cs
@@ -0,0 +1,102 @@
+﻿/* №1
+int [] Array = new int[10];
+Random rand = new Random();
+for (int i = 0; i<10; i++)
+{
+    Array[i] = rand.Next(-100,100);
+    Console.Write(Array[i] + " ");
+}
+  
+int minElem = 100;
+int minEmI = 0;
+for (int i = 0; i<10; i++)
+{
+    if (Array[i] < minElem) minElem = Array[i];
+    if (Array[i] < minEmI) minEmI = i;
+}
+Console.WriteLine($"\n {minEmI}");
+*/
+
+/* №2 
+List<int> nums = new List<int>();
+Console.WriteLine("Введите числа (для завершения введите 0)");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int m = 1;
+while (num != 0)
+{
+    nums.Add(num);
+    sum += num;
+    m *= num;
+    
+    num = Convert.ToInt32(Console.ReadLine());
+}
+int avg = sum / nums.Count;
+Console.WriteLine(sum);
+Console.WriteLine(m);
+Console.WriteLine(avg);
+*/
+
+/* №3
+List<string> s = new List<string>(); 
+Console.WriteLine("Введите слова (для завершения введите Enter)"); 
+string str = Console.ReadLine(); 
+string korotkoe = str; 
+string dlinoe = str; 
+ 
+while (str != "") 
+{ 
+    s.Add(str); 
+    if (str.Length < korotkoe.Length) 
+    { 
+        korotkoe = str; 
+    } 
+    if (str.Length > dlinoe.Length) 
+    { 
+        dlinoe = str; 
+    }
+    str = Console.ReadLine(); 
+} 
+Console.WriteLine(korotkoe);
+*/
+
+/* №4
+namespace ConsoleApp1;
+
+public class Program 
+{
+    static int[] Filled(int a, int b)
+    {
+        int[] arr = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            arr[i] = rand.Next(a, b);
+        }
+        return arr;
+    }
+    static void Main()
+    {
+        int[] filledArray = Filled(0, 100);
+        for (int i = 0; i < filledArray.Length; i++) 
+        {
+            Console.Write($"{filledArray[i]} ");
+        }
+    }
+}
+
+*/
+
+/* №5
+Console.WriteLine("Введите текст: "); 
+string str = Console.ReadLine(); 
+int probel = 1; 
+foreach (char sym in str) 
+{ 
+ if (sym == ' ') probel++; 
+} 
+Console.WriteLine(probel); 
+Console.WriteLine($"Start {str} End");
+*/
+
+Console.WriteLine();
---
+++ rabota_1.2/Program.cs
@@ -0,0 +1,125 @@
+﻿/* №1
+ using System.Collections;
+namespace ConsoleApp1;
+public class Program 
+{
+    static void Main()
+    {
+        List<int> nums = new List<int>(100);
+        int a = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < 100; i++)
+        {
+            nums.Add(a);
+            a -= 3;
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write($"{nums[i]} ");
+        }
+    }
+} */
+/* №2 
+namespace ConsoleApp1;
+public class Program 
+{
+    static void Main()
+    {
+        int[] array = new int[100];
+        int a = 1;
+        for (int i = 0; i < 100; i++)
+        {
+            array[i] = a;
+            a += 2;
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+} */
+/* №3
+using System.Collections;
+namespace ConsoleApp1;
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите размер матрицы: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            array[0, i] = 1;
+            array[i, 0] = 1;
+        }
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                array[i, j] = array[i - 1, j] + array[i, j - 1];
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+} */
+
+
+static int[] Avgtemperature(int[,] temperature)
+{
+    int[] Avgtemper = new int [12];
+    int avg = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 30; j++)
+        {
+            avg += temperature[i, j];
+        }
+
+        Avgtemper[i] = avg / 30;
+        avg = 0;
+    }
+
+    return Avgtemper;
+}
+
+int[,] temperature = new int[12, 30];
+int min = -20 ;
+int max = 20;
+Random rand = new Random();
+for (int i = 0; i < 12; i++)
+{ 
+    for (int j = 0; j < 30; j++)
+    {
+        temperature[i, j] = rand.Next(min, max);
+    }
+
+    if (i<6)
+    {
+        min += 3;
+    }
+
+    else if (i>6)
+    {
+        max += 3;
+    }
+}
+for (int i = 0; i < 12; i++)
+{
+    Console.WriteLine($"Месяц №{i}");
+    for (int j = 0; j < 30; j++)
+    {
+        Console.Write($"{temperature[i, j]} ");
+    }
+    
+    
+    Console.WriteLine();
+}
